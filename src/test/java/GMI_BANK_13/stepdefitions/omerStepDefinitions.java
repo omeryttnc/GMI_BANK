476177a730275ff943e2b72e022c2bbb3af7a0dd
omeryttnc <67800186+omeryttnc@users.noreply.github.com>
2020-12-01 19:56:53 +0000

    deneme

--- src/test/java/GMI_BANK_13/stepdefitions/omerStepDefinitions.java
+++ src/test/java/GMI_BANK_13/stepdefitions/omerStepDefinitions.java
@@ -1,4 +1,10 @@
 package GMI_BANK_13.stepdefitions;
 
+import org.junit.Test;
+
 public class omerStepDefinitions {
+    @Test
+    public void deneme(){
+        System.out.println("deneme");
+    }
 }
