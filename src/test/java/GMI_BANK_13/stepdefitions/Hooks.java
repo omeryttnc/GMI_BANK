1e5363dedc6808c47b36ce42f713a79a7affc3f5
maliayyildiz <you@example.com>
2020-12-04 22:46:43 +0100

    hooks class and plugins to pom.xml

---
+++ src/test/java/GMI_BANK_13/stepdefitions/Hooks.java
@@ -0,0 +1,23 @@
+package GMI_BANK_13.stepdefitions;
+
+import GMI_BANK_13.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class Hooks {
+    //    @Before
+//    public void setUp(){
+//    }
+
+    @After
+    public void tearDown(Scenario scenario){
+        final byte[] screenshot=((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+        if (scenario.isFailed()) {
+            scenario.attach(screenshot, "image/png","screenshots");
+        }
+        Driver.closeDriver();
+    }
+
+}
