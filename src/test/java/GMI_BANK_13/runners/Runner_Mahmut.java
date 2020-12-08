7a0d762a7e28869f0eeef75708406cb06b5f7f14
Mahmut Bal <furkanmuratbal@gmail.com>
2020-12-08 22:06:52 +0100

    that's my first commit at mahoney branch

--- src/test/java/GMI_BANK_13/runners/Runner_Mahmut.java
+++ src/test/java/GMI_BANK_13/runners/Runner_Mahmut.java
@@ -1,4 +1,20 @@
 package GMI_BANK_13.runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"html:target/default-cucumber-reports",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml"},
+        features = "src/test/resources/features",
+        glue = "GMI_BANK_13/stepdefitions",
+        tags = "@registration",
+        dryRun = false,
+        strict = true
+)
 public class Runner_Mahmut {
+
 }
